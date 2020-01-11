731f7550f746cd41ef63b24af8356da1c5a537d3
n1tjrgns <n2tjrgns@gmail.com>
2020-01-11 17:07:46 +0900

    AOP의 Porxy 패턴 1 - Pay 방식을 class에 전가

---
+++ src/main/java/org/springframework/samples/petclinic/proxy/Store.java
@@ -0,0 +1,17 @@
+package org.springframework.samples.petclinic.proxy;
+
+public class Store {
+
+    Payment payment;
+
+    //생성자를 통한 의존성 주입
+    //store 클래스는 payment 없이는 동작 할 수 없다. 강제성성
+   public Store(Payment payment) {
+        this.payment = payment;
+    }
+
+    public void buySomething(){
+        payment.pay(100);
+    }
+
+}
